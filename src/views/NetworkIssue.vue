df36b7fac65854ae8e359a8173091387d5698633
lucasfloriani <lucas.floriane@gmail.com>
2019-02-26 22:53:59 -0300

    Add message in network issue view content

--- src/views/NetworkIssue.vue
+++ src/views/NetworkIssue.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Network Issues</div>
+  <v-container fill-height>
+    <v-layout align-center>
+      <v-flex xs12>
+        <v-card class="pa-4">
+          <h1 class="display-3">Uh-Oh!</h1>
+          <h3
+            class="title"
+          >Parece que você está experenciando problemas com conexão, por favor clique no botão de voltar e tente novamente.</h3>
+          <router-link :to="{ name: 'home-page' }">
+            <v-btn color="info" class="ma-0 mt-4">Ou volte para a home page</v-btn>
+          </router-link>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
