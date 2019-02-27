49ba50a9453057c3f7d6cb5d6d7b9e6e6ecb8b49
lucasfloriani <lucas.floriane@gmail.com>
2019-02-26 22:54:08 -0300

    Add message in not found view content

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Not Found</div>
+  <v-container fill-height>
+    <v-layout align-center>
+      <v-flex xs12>
+        <v-card class="pa-4">
+          <h1 class="display-3">Oops!</h1>
+          <h3 class="title">O que você estava procurando não é aqui.</h3>
+          <router-link :to="{ name: 'home-page' }">
+            <v-btn color="info" class="ma-0 mt-4">Voltar para home page</v-btn>
+          </router-link>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
